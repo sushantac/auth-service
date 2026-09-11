6530200ef286492a2db9ed5629b63b77bbf64ea8
Sushant <sushantac@gmail.com>
2026-09-11 22:07:55 +1000

    fix(config): disable Kafka auto-configuration

--- src/main/java/com/ecommerce/auth/AuthApplication.java
+++ src/main/java/com/ecommerce/auth/AuthApplication.java
@@ -2,8 +2,9 @@ package com.ecommerce.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = KafkaAutoConfiguration.class)
 public class AuthApplication {
 
     public static void main(String[] args) {
